b65b71eb190cae54f411e90058413ad3910b0b97
Ahmad Hilman Dani <ahmadhilmanlagi@gmail.com>
2023-06-25 13:51:09 +0700

    UPDATE add mongoDB and sucessfully insert data

--- server/routes/main.js
+++ server/routes/main.js
@@ -1,14 +1,25 @@
 const express = require('express')
 
 const router = express.Router()
+const post = require('../models/post')
 
 router.get('', (req, res) => {
   res.render('index')
 })
 
-
 router.get('/about', (req, res) => {
   res.render('about')
 })
+
+// function insertPostData() {
+//   post.insertMany([
+//     {
+//       title: "Building a blog",
+//       body: "Node JS, Express, Mongo",
+//     }
+//   ])
+// }
+// insertPostData();
+
 
 module.exports = router
